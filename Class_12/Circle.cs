c7ac42db138ebb9d3ee269dd24c098ade0576b54
Irina <spartak.irina92@gmail.com>
2022-01-02 16:28:08 +0700

    Добавьте файлы проекта.

---
+++ Class_12/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_12
+{
+    static class Circle // если класс полностью статический, все методы у него статические, 
+                        // то не нужно делать у него статических полей
+    {
+        static public double Getlenght(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI*r * r;
+        }
+        static public string GetAffiliation(double x, double y, double x0, double y0, double r)
+        {
+            if (Math.Sqrt((x - x0) * (x - x0) + (y - y0) - (y - y0)) <= r)
+            {
+                return ("точка принадлежит кругу");
+
+            }
+            else
+            {
+                return ("точка не принадлежит кругу");
+            }
+        }
+    }
+}
+
+
---
+++ Class_12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ВВедите радиус окружности r=");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double lenght = Circle.Getlenght(r);
+            double square = Circle.GetSquare(r);
+            Console.WriteLine($"Длина окружности={lenght:0.000} \nПлощадь круга={square:0.000}");
+
+            Console.WriteLine();
+            Console.WriteLine("ВВедите координаты центра окружности x0, y0:");
+            Console.Write("ВВедите координату x0=");
+            double x0= Convert.ToDouble(Console.ReadLine());
+            Console.Write("ВВедите координату y0=");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("ВВедите координаты точки x, y:");
+            Console.Write("ВВедите координату x=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ВВедите координату y=");
+            double y = Convert.ToDouble(Console.ReadLine());
+            string GetAffiliation = Circle.GetAffiliation(x, y, x0, y0,r);
+            Console.WriteLine($"\nПринадлежность точки к кругу - {GetAffiliation}");
+
+            Console.ReadKey();
+        }
+    }
+}
